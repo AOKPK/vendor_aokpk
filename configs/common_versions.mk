35883f9e918ee3ddb4485a9749702e8daa1fc6ca
kecinzer <kecinzer@gmail.com>
2013-04-30 11:55:39 +0200

    Change version definition
    Signed-off-by: kecinzer <kecinzer@gmail.com>

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -3,22 +3,21 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_VERSION_TAGS=release-keys USER=android-bui
 
 DATE = $(shell vendor/aokp/tools/getdate)
 DEV = $(shell echo $(TARGET_PRODUCT) | cut -f2 -d '_')
-#AOKP_BUILD = $(shell echo '1')
+#AOKPK_STABLE = $(shell echo '2')
 
-ifneq ($(AOKP_BUILD),)
-	# AOKP_BUILD=<goo version int>/<build string>
-	PRODUCT_PROPERTY_OVERRIDES += \
-	    ro.goo.developerid=kecinzer \
-	    ro.goo.rom=aokpk \
-	    ro.goo.version=$(AOKP_BUILD) \
-		ro.aokp.version=aokpk_$(DEV)_stable_$(AOKP_BUILD)
+ifneq ($(AOKPK_STABLE),)
+  PRODUCT_PROPERTY_OVERRIDES += \
+    ro.goo.developerid=kecinzer \
+    ro.goo.rom=aokpk \
+    ro.goo.version=$(AOKPK_STABLE) \
+    ro.aokp.version=aokpk_$(DEV)_stable_$(AOKPK_STABLE)
 else
   DATETIME=$(shell date +"%y%m%d%H%M")
-	PRODUCT_PROPERTY_OVERRIDES += \
-      ro.goo.developerid=kecinzer \
-      ro.goo.rom=aokpkdaily \
-      ro.goo.version=$(DATETIME) \
-      ro.aokp.version=aokpk_$(DEV)_daily_$(DATETIME)
+  PRODUCT_PROPERTY_OVERRIDES += \
+    ro.goo.developerid=kecinzer \
+    ro.goo.rom=aokpkdaily \
+    ro.goo.version=$(DATETIME) \
+    ro.aokp.version=aokpk_$(DEV)_daily_$(DATETIME)
 endif
 
 # Camera shutter sound property
