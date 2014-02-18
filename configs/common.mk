8f7ace71d0f992403d7a909893e7dc7a494c4e01
kecinzer <kecinzer@gmail.com>
2014-02-18 09:59:36 +0100

    Removed LockClock widget
    Since transparent lockscreen its unusable for me and can be downloaded here
    https://play.google.com/store/apps/details?id=com.dvtonder.chronus
    
    Signed-off-by: kecinzer <kecinzer@gmail.com>

--- configs/common.mk
+++ configs/common.mk
@@ -20,10 +20,6 @@ PRODUCT_PACKAGES += \
     UnicornPorn \
     KitKatWhite
 
-# Extra Packages
-PRODUCT_PACKAGES += \
-    LockClock \
-
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.clientidbase=android-google
